5165545a3bad87c08a6e918a2965d96ce3b71db0
Krishnamanoj Varanasi <Krishnamanoj.Varanasi@infor.com>
2017-01-10 09:56:06 +0530

    updated yts.js

--- yts.js
+++ yts.js
@@ -5,19 +5,15 @@ var YTS = function(){
 	this.baseUrl = "http://yts.ag/api/v2";
 
 	this.getMovies = function(page = 1, year, limit = 20){
-		if(isNaN(year)){
-			return "${year} is not a number";
-		}else{
-			return request({
-				uri: this.baseUrl + "/list_movies.json",
-				qs: {
-					page: page,
-					year: year || new Date().getFullYear(),
-					limit: limit
-				},
-				json: true
-			});
-		}
+		return request({
+			uri: this.baseUrl + "/list_movies.json",
+			qs: {
+				page: page,
+				year: year || new Date().getFullYear(),
+				limit: limit
+			},
+			json: true
+		});
 	};
 	this.search = function(query){
 		return request({
