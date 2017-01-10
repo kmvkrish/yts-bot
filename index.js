f05fcbc56e6b6222cc692ee0f79bae9e0d1f6ff9
Krishna Manoj Varanasi <krishnamanojvaranasi.kmv@gmail.com>
2017-01-10 21:03:15 +0530

    updated bot

--- index.js
+++ index.js
@@ -5,31 +5,54 @@ var app = express()
 
 var yts = require("./yts").yts;
 
+var token = "EAARms7g6rZAUBALWlSsazZCmL2mc8WZCHlfHNKPT2glmGS2aq5MGqfZAkUYDvdYYIZAw531L0FpHHvNhi38l8m13LHTWUKbp3CuLwUZAP1qn12dCinCPhBhQOkLYjl8UI5BgMtZANvsZAlCxEs777CUUoMGxDTX4sZCTWXEuKTcnzFgZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
 app.set('port', (process.env.PORT || 5000));
 
-// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 
-// Process application/json
 app.use(bodyParser.json());
 
-// Index route
 app.get('/', function (req, res) {
-    //res.send('Hello world, I am a chat bot');
     yts.getMovies().then((response) => {
     	res.send(response.body.data);
     });
 });
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
     }
-    res.send('Error, wrong token')
-})
+    res.sendStatus(200)
+});
 
-// Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
-})
+});
